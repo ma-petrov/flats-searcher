a74afd6b48b91a3bb83c33d601f12e3805c4ae2b
Petrov Mikhail <petrov.ma@icloud.com>
2024-11-12 21:05:06 +0300

    fix telegram exceptions

--- src/telegram.py
+++ src/telegram.py
@@ -8,10 +8,7 @@ from conf import TELEGRAM_URL, TG_TOKEN, TG_CHAT_ID, DEBUG
 logger = get_logger(__name__)
 
 
-def send_telegram(text: str):    
-    class TelegramError(Exception):
-        ...
-
+def send_telegram(text: str):
     url = TELEGRAM_URL + TG_TOKEN + '/sendMessage'
     data = {'chat_id': TG_CHAT_ID, 'text': text}
 
@@ -19,8 +16,12 @@ def send_telegram(text: str):
         logger.info("MESSAGE_SENT", text=text)
         return
     
-    response = requests.post(url, data=data)
+    try:
+        response = requests.post(url, data=data)
+    except ConnectionError:
+        logger.error("TELEGRAM_ERROR", type="CONNECTION")
+    except Exception:
+        logger.exception("TELEGRAM_ERROR", type="UNKNOWN")
     
     if response.status_code != 200:
-        logger.error("TELEGRAM_ERROR")
-        raise TelegramError(response.text)
+        logger.error("TELEGRAM_ERROR", type="RESPONSE")
